5b1c0c9f25b4f4385451bab883cc8f6aba15cd06
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-04-02 15:35:49 -0300

    class que é criado os elementos no canvas

---
+++ apis/Element.js
@@ -0,0 +1,24 @@
+class Element {
+    constructor(name) {
+        this.cnv = document.querySelector('#canvas-main')
+        this.ctx = this.cnv.getContext('2d')
+
+        if(name !== null) {
+            this.datas = Data.find(name)
+        }
+    }
+
+    rect() {
+        let datas = this.datas;
+        let ctx = this.ctx
+        ctx.fillStyle = datas.color
+        ctx.fillRect(datas.x, datas.y, datas.width, datas.height)
+        ctx.fill()
+    }
+
+    clearAll() {
+        let ctx = this.ctx
+        let cnv = this.cnv
+        ctx.clearRect(0,0,cnv.width,cnv.height)
+    }
+}
