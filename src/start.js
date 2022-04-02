98223a22ec3d05b97e151ecd05268aeda9edf07e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-04-02 16:07:54 -0300

    add dados do elemento fruta

--- src/start.js
+++ src/start.js
@@ -5,9 +5,12 @@
 
 function start() {
     new Data('player')
-        .rect(10, 10, 30, 30, 'blue')
-        .speed(10, 10)
+        .rect(10, 10, 20, 20, 'blue')
+        .speed(6, 6)
 
     new Data('parede')
-        .rect(230, 130, 20, 80, 'black')
+        .rect(230, 130, 10, 100, 'black')
+
+    new Data('fruta')
+        .rect(60,220,25,25,'green')
 }
