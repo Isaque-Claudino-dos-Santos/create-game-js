e9d435ca835abbcfe9a3874bbcf791d8672b363a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-04-02 16:08:07 -0300

    add evento apos a coleta da fruta

--- src/update.js
+++ src/update.js
@@ -1,9 +1,15 @@
 /** update()
  * - Executa em varias atualizações 
  * - Para atualizações do conteudo da tela 
+ * - Elemento que fica por cima é utima a ser escrita!
  */
 
 function update() {
     new Moviment('player').controllers(['w', 'd', 's', 'a'])
-    new Collider('player','parede').solid()
+    new Collider('player', 'parede').solid()
+
+    new Collider('player', 'fruta').hover((player,fruta) => {
+        player.color = 'red'
+        fruta.visible = 0
+    })
 }
