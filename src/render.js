f947aa8fbc52c13f7076584df4548a28eca50d1b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-04-02 15:38:11 -0300

    arquivo que tem a função render em que exibira o conteudo na tela

---
+++ src/render.js
@@ -0,0 +1,9 @@
+/** render()
+ * - Executa em varias atualizações 
+ * - Para renderizaçãoes visuais na tela 
+ */
+
+function render() {
+    new Element('player').rect()
+    new Element('parede').rect()
+}
