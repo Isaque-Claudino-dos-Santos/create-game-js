ede4ffff4e812984b4ffdc09b7a213d3b4f6912f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-04-15 14:37:55 -0300

    new Element score

--- src/render.js
+++ src/render.js
@@ -4,9 +4,12 @@
  */
 
 function render() {
-    new Cam('player',0.25)
+    new Cam('player', 0.25)
     new Element('paredes').group()
     new Element('fruta').image()
     new Element('player').rect()
-    
+
+    new Element('score')
+        .text()
+        .relative(10, 20)
 }
