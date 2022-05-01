6f5959c38732b50fc347e36744817b6b8d334f28
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-01 16:18:10 -0300

    reder parede e fruta

--- src/render.js
+++ src/render.js
@@ -1,4 +1,7 @@
 function render() {
     draw.render('rect', player1)
-    draw.render('rect',player2)
+    draw.render('rect', player2)
+    draw.render('rect', parede)
+    draw.render('rect', fruta)
+
 }
