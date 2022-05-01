3d6aa863cb73a6d59e9282497dda3d999ba506ba
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-01 15:26:15 -0300

    define dois controlles para os jogadores

--- src/start.js
+++ src/start.js
@@ -1,24 +1,12 @@
+var player1 = data.rect(10, 10, 30, 30, 'red')
+player1 = data.set(player1, data.speed(4))
+
+var player2 = data.rect(300, 10, 30, 30, 'blue')
+player2 = data.set(player2, data.speed(4))
 
 
-var player = data.rect(10, 10, 30, 30, 'blue')
-player = data.set(player, data.speed(4))
-
-var paredes = data.rect(150, 100, 10, 90, 'red')
 
 function start() {
-    key.keyboard('press.w',() => {
-        player.y -= player.speedY
-    })
-
-    key.keyboard('press.d',() => {
-        player.x += player.speedX
-    })
-    
-    key.keyboard('press.s', () => {
-        player.y += player.speedY
-    })
-
-    key.keyboard('press.a',() => {
-        player.x -= player.speedX
-    })
+    vector.moviment(player1, 'w', 'd', 's', 'a')
+    vector.moviment(player2, 'ArrowUp', 'ArrowRight', 'ArrowDown', 'ArrowLeft')
 }
