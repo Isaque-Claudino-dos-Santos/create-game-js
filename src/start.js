fea8e15906804d9f5c4b6bdcca20a70bc8ef3f09
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-01 16:18:18 -0300

    datas

--- src/start.js
+++ src/start.js
@@ -1,10 +1,11 @@
-var player1 = data.rect(10, 10, 30, 30, 'red')
+var player1 = data.rect(10, 10, 20, 20, 'red')
 player1 = data.set(player1, data.speed(4))
 
-var player2 = data.rect(300, 10, 30, 30, 'blue')
+var player2 = data.rect(300, 10, 20, 20, 'blue')
 player2 = data.set(player2, data.speed(4))
 
-
+var parede = data.rect(200, 100, 10, 100)
+var fruta = data.rect(200, 30, 15, 15, 'green')
 
 function start() {
     vector.moviment(player1, 'w', 'd', 's', 'a')
