6daec72a5d4f92af0422a8873cd0cfe3594abcf4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-01 15:08:31 -0300

    tudo foi apagado para recriar

--- src/update.js
+++ src/update.js
@@ -1,31 +1,3 @@
-/** update()
- * - Executa em varias atualizações 
- * - Para atualizações do conteudo da tela 
- * - Elemento que fica por cima é utima a ser escrita!
- */
-
 function update() {
-    new Moviment('player').controllers(['w', 'd', 's', 'a'])
 
-    Data.find('paredes').forEach(parede => {
-        new Collider('player', parede).solid()
-    });
-
-    new Collider('player', 'fruta').hover((player, fruta) => {
-        if (fruta.visible) {
-            player.color = 'red'
-            fruta.visible = 0
-            fruta.score += 1
-            console.log('Que fruta Boa')
-
-            Data.update('score', (s) => {
-                s.text = 'SCORE:' + fruta.score
-            })
-
-            setTimeout(() => {
-                fruta.visible = 1
-                console.log('Respawn Fruit')
-            }, 1000)
-        }
-    })
 }
