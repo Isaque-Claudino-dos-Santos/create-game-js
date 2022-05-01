dd93f4bbad6f71b4cf3b90be2f5eb273991957fe
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-01 15:07:51 -0300

    desenhado o player e parede

--- src/render.js
+++ src/render.js
@@ -1,15 +1,4 @@
-/** render()
- * - Executa em varias atualizações 
- * - Para renderizaçãoes visuais na tela 
- */
-
 function render() {
-    new Cam('player', 0.25)
-    new Element('paredes').group()
-    new Element('fruta').image()
-    new Element('player').rect()
-
-    new Element('score')
-        .text()
-        .relative(10, 20)
+    draw.render('rect', player)
+    draw.render('rect',paredes)
 }
