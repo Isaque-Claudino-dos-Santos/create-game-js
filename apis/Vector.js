19e1993583f00c726dec36872d0b8524ec70cc86
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-07 19:24:03 -0300

    arumando escrita

--- apis/Vector.js
+++ apis/Vector.js
@@ -1,19 +1,19 @@
 class Vector {
     moviment(nameData, TOP_CONTROLLER, RIGHT_CONTROLLER, DOWN_CONTROLLER, LEFT_CONTROLLER) {
         let datas = data.find(nameData)
-        key.keyboard('press.' + TOP_CONTROLLER, () => {
+        key.keyboard(TOP_CONTROLLER, () => {
             datas.y -= datas.speedY
         })
 
-        key.keyboard('press.' + RIGHT_CONTROLLER, () => {
+        key.keyboard(RIGHT_CONTROLLER, () => {
             datas.x += datas.speedX
         })
 
-        key.keyboard('press.' + DOWN_CONTROLLER, () => {
+        key.keyboard(DOWN_CONTROLLER, () => {
             datas.y += datas.speedY
         })
 
-        key.keyboard('press.' + LEFT_CONTROLLER, () => {
+        key.keyboard(LEFT_CONTROLLER, () => {
             datas.x -= datas.speedX
         })
     }
