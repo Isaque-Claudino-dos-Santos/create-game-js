5654071c40f71d0ca3af377ff41c9c8506bcf9d0
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-08 14:09:23 -0300

    add  metodo text for draw text

--- apis/Draw.js
+++ apis/Draw.js
@@ -12,6 +12,9 @@ class Draw {
                     case 'image':
                         this.image(datas)
                         break
+                    case 'text':
+                        this.text(datas)
+                        break
                 }
             } else {
                 throw "The type data ->" + datas.type + "<- not corrensponds to the method ->" + type + "<- required"
@@ -19,11 +22,12 @@ class Draw {
     }
 
     rect(datas) {
-        CONTEXT.fillStyle = datas.color
         if (datas.fill) {
+            CONTEXT.fillStyle = datas.color
             CONTEXT.fillRect(datas.x, datas.y, datas.width, datas.height)
             CONTEXT.fill()
         } else {
+            CONTEXT.strokeStyle = datas.color
             CONTEXT.strokeRect(datas.x, datas.y, datas.width, datas.height)
             CONTEXT.stroke()
         }
@@ -31,6 +35,20 @@ class Draw {
 
     image(datas) {
         CONTEXT.drawImage(datas.src, datas.x, datas.y, datas.width, datas.height)
+    }
+
+    text(datas) {
+        CONTEXT.font = datas.font
+        CONTEXT.textBaseline = 'top'
+        if (datas.fill) {
+            CONTEXT.fillStyle = datas.color
+            CONTEXT.fillText(datas.text, datas.x, datas.y)
+            CONTEXT.fill()
+        } else {
+            CONTEXT.strokeStyle = datas.color
+            CONTEXT.strokeText(datas.text, datas.x, datas.y)
+            CONTEXT.stroke()
+        }
     }
 
     clearCANVAS() {
