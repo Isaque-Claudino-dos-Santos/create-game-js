e72b7b73d33f3c7165954146641d188bee890910
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-15 19:25:09 -0300

    create group

--- src/start.js
+++ src/start.js
@@ -2,19 +2,24 @@ function start() {
     // PLAYER
     data.rect('player', 10, 10, 20, 20, 'red')
     data.update('player', data.speed(6))
-    // BLOCO
-    data.rect('bloco', 400, 100, 10, 100)
+    // PARADES
+    data.group('paredes', (d) => {
+        return [
+            d.rect(null, 100, 10, 30, 300),
+            d.rect(null, 0, 300, 100, 30)
+        ]
+    })
     //FRUTA
-    data.image('fruta', 100, 100, 16, 16, 'cogumelo.png')
+    data.image('fruta', 30, 100, 16, 16, 'cogumelo.png')
     //score
-    data.text('score', 0, 0, 20,'SCORE: ')
+    data.text('score', 0, 0, 20, 'SCORE: ')
 
-     cam.activeCam('player', .25)
+    cam.activeCam('player', .25)
 
     vector.moviment('player', 'w', 'd', 's', 'a')
 
-    key.keyboard('Enter', (k) => {
-        k.constant = false
+    key.keyboard('Enter', (dataKey) => {
+        dataKey.constant = false
         console.log('start')
     })
 }
