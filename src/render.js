6542b0434e8a0228fc2939e760760ac9cd3a4b93
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-15 19:25:01 -0300

    render group

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
 function render() {
-    draw.render('rect',['player','bloco'])
+    draw.render('rect',['player','paredes'])
     draw.render('text', 'score')
         .relative('score',0,0)
     draw.render('image', 'fruta')
