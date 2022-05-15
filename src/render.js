568c93b357fa0219115695c2ea53350bf460e208
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-14 23:07:34 -0500

    use method render in group

--- src/render.js
+++ src/render.js
@@ -1,5 +1,5 @@
 function render() {
-    draw.renderGroup('rect',['player','bloco'])
+    draw.render('rect',['player','bloco'])
     draw.render('text', 'score')
         .relative('score',0,0)
     draw.render('image', 'fruta')
