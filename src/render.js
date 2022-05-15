a4bba76ba6acbbe1942e17cfa38337f4d7f00b3f
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-14 22:01:21 -0500

    call relative method

--- src/render.js
+++ src/render.js
@@ -1,6 +1,7 @@
 function render() {
     draw.render('rect', 'player')
     draw.render('text', 'score')
+        .relative('score',20,20)
     draw.render('rect', 'bloco')
     draw.render('image', 'fruta')
 }
