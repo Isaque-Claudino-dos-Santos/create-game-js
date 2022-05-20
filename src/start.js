f187de85851d039071c1d074c50ffaae21a63b5e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-20 17:09:06 -0300

    create data player set speed and moviment, and data paredes

--- src/start.js
+++ src/start.js
@@ -1,3 +1,21 @@
 function start() {
-   
+    data.rect('player', WIDTH / 2, HEIGHT / 2, 16, 16, 'black')
+    data.speed('player', 8)
+    vector.moviment('player', 'w', 'd', 's', 'a')
+
+    data.group('paredes', (d) => {
+        let borderSize = 10
+        let borderColor = 'grey'
+        return [
+            //top
+            d.rect(null, 0, 0, WIDTH, borderSize, borderColor),
+            //left
+            d.rect(null, 0, borderSize, borderSize, HEIGHT, borderColor),
+            //right
+            d.rect(null, WIDTH - borderSize, borderSize, borderSize, HEIGHT, borderColor),
+            //bottom
+            d.rect(null, 0, HEIGHT - borderSize, WIDTH, borderSize, borderColor)
+        ]
+    })
+
 }
