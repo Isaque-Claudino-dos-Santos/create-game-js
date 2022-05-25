c9e35cc7bc477c122641bd12dbe27975492df55b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-25 17:19:47 -0300

    auter color top parede e cor das paredes

--- src/start.js
+++ src/start.js
@@ -1,14 +1,16 @@
 function start() {
-    data.rect('player', 30, 30, 16, 16, 'black')
-    data.speed('player', 8)
+    data.rect('player', 40, 40, 16, 16, 'blue')
+    data.speed('player', 5)
+    data.addProp('player', { pontos: 0 })
+
     vector.moviment('player', 'w', 'd', 's', 'a')
 
     data.group('paredes', (d) => {
         let borderSize = 10
-        let borderColor = 'grey'
+        let borderColor = 'black'
         return [
-            //top
-            d.rect(null, 0, 0, WIDTH, borderSize, borderColor),
+            //top == HUD
+            d.rect(null, 0, 0, WIDTH, borderSize * 3, borderColor),
             //left
             d.rect(null, 0, borderSize, borderSize, HEIGHT, borderColor),
             //right
@@ -19,4 +21,5 @@ function start() {
     })
 
     data.rect('fruta', WIDTH / 2, HEIGHT / 2, 12, 12, 'green')
+    data.text('pontos', 12, 7, 20)
 }
