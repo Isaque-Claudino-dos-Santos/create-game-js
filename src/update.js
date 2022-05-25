aeddee6ac57930fcd1de44e8b0b9340152091db8
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-25 17:20:24 -0300

    coleta da fruta, atualização de pontos e randomização do respown da fruta

--- src/update.js
+++ src/update.js
@@ -1,3 +1,23 @@
 function update() {
     collider.set('solid', 'player', 'paredes')
+
+    collider.set('hover', 'player', 'fruta', (p, f) => {
+        if (f.visible) {
+            p.pontos += 1
+
+            f.visible = false
+            setTimeout(() => {
+                f.x = rnValue(WIDTH)
+                f.y = rnValue(HEIGHT)
+                f.visible = true
+            }, 350)
+        }
+    })
+
+    data.update('pontos', (text) => {
+        let player = data.find('player')
+        text.text = 'Pontos: ' + player.pontos
+        text.fontFamily = 'Consolas'
+        text.color = 'white'
+    })
 }
