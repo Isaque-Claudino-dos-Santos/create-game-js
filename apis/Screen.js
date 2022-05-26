b16e5c0c53c7fe65022cfaa082780ceb65541334
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-26 11:26:10 -0300

    class to create screen

---
+++ apis/Screen.js
@@ -0,0 +1,48 @@
+class Screen {
+    constructor() {
+        this.screens = {}
+    }
+
+    setScreens(screenName, datas) {
+        this.screens[screenName] = datas
+    }
+
+    create(screenName, dataName) {
+        if (isString(screenName)) {
+            this.setScreens(screenName, { datas: this.setDatasInScreen(dataName), screenVisible: false })
+        }
+    }
+
+    setDatasInScreen(dataName) {
+        if (isString(dataName)) {
+            return data.find(dataName)
+        }
+
+        if (isArray(dataName)) {
+            return dataName
+        }
+    }
+
+    visible(screenName, isVisible) {
+        if (isString(screenName) && isBool(isVisible)) {
+            let screen = this.find(screenName)
+            screen.screenVisible = isVisible
+        }
+    }
+
+    find(screenName) {
+        let screenData = this.screens[screenName]
+        if (isUndefined(screenData)) {
+            throw "This name -> " + screenName + " <- not existent for find screen"
+        } else {
+            return screenData
+        }
+    }
+
+    mod(screenName, callbakc) {
+        if (isString(screenName) && isFunc(callbakc)) {
+            let screen = this.screens[screenName]
+            callbakc(screen)
+        }
+    }
+}
