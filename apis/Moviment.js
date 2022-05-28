3e2f721da413c40ee8d0df4e6b68266af1f8de3d
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-28 13:14:42 -0300

    add moviment click

--- apis/Moviment.js
+++ apis/Moviment.js
@@ -1,11 +1,15 @@
 class Moviment {
     #controllers = {}
 
+    #getController() {
+        return this.#controllers
+    }
+
     #getControllerByName(name) {
         return this.#controllers[name]
     }
 
-    #setControllersWithName(name, datas) {
+    #setControllersByName(name, datas) {
         this.#controllers[name] = datas
     }
 
@@ -16,102 +20,62 @@ class Moviment {
      */
 
     #callMethodByName(methodName, datasName) {
-        eval('this.#' + methodName + '_moviment(datasName)')
+        eval('this.#' + methodName + '(datasName)')
     }
 
-    /**
-     * aplay moviment constant of the press key
-     * @param {string} dataName 
-     */
-
-    #press_moviment(dataName) {
-        let datas = this.#getControllerByName(dataName)
-        let movimentKeys = datas.movimentKeys
-        key.keyboard(movimentKeys.top, () => {
-            datas.y -= datas.speedY
-            datas.stopped = false
-        })
-
-        key.keyboard(movimentKeys.right, () => {
-            datas.x += datas.speedX
-            datas.stopped = false
-        })
-
-        key.keyboard(movimentKeys.down, () => {
-            datas.y += datas.speedY
-            datas.stopped = false
-        })
-
-        key.keyboard(movimentKeys.left, () => {
-            datas.x -= datas.speedX
-            datas.stopped = false
-        })
-    }
-
-    /**
-     * aplay moviment for click
-     * @param {string} dataName 
-     */
-
-    #click_moviment(dataName) {
-        let datas = this.#getControllerByName(dataName)
-        let movimentKeys = datas.movimentKeys
-
+    #click() {
         let moviment = {
-            on: true,
+            keydown: false,
 
-            top: () => {
+            top: (datas) => {
                 datas.y -= datas.speedY
-                datas.stopped = false
             },
-            left: () => {
+            left: (datas) => {
                 datas.x -= datas.speedX
-                datas.stopped = false
             },
-            right: () => {
-                datas.x += datas.speedX
-                datas.stopped = false
-            },
-            down: () => {
+            down: (datas) => {
                 datas.y += datas.speedY
-                datas.stopped = false
-            }
+            },
+            right: (datas) => {
+                datas.x += datas.speedX
+            },
         }
 
-        addEventListener('keydown', (e) => {
-            for (const methodName in movimentKeys) {
-                let key = movimentKeys[methodName]
-                if (e.key === key && !isUndefined(key)) {
-                    let method = moviment[methodName]
-                    if (!isUndefined(method)) {
-                        if (moviment.on) {
-                            method()
-                            moviment.on = false
-                        }
-                    }
-                }
-            }
-        })
+        for (const nameController in this.#getController()) {
+            let constroller = this.#getControllerByName(nameController)
+            let movimentKeys = constroller.movimentKeys
 
-        addEventListener('keyup', (e) => {
-            moviment.on = true
-        })
+            key.create('moviment_click')
+                .keydown((event) => {
+                    if (!moviment.keydown) {
+                        for (const nameMethod in movimentKeys) {
+                            if (event.key === movimentKeys[nameMethod]) {
+                                moviment[nameMethod](constroller)
+                            }
+                        }
+                        moviment.keydown = true
+                    }
+                })
+                .keyup(() => {
+                    moviment.keydown = false
+                })
+                .save()
+        }
     }
 
-    /**
-    * Define set datas in constrollers
-    * @param {object} datas 
-    */
+    #press() {
+        for (const nameController in this.#getController()) {
+            let constroller = this.#getControllerByName(nameController)
+            key.create('moviment_press')
+                .keydown((event) => { })
+                .keyup((event) => { })
+                .save()
+        }
+    }
 
     set(datas) {
-        this.#setControllersWithName(datas.name, datas)
-        this.#callMethodByName(datas.typeMoviment, datas.name)
-    }
-
-    mod(constrollerName, callback) {
-        if (isFunc(callback) && isString(constrollerName)) {
-            callback(this.#getControllerByName(constrollerName))
-        }
+        this.#setControllersByName(datas.name, datas)
+        this.#callMethodByName(datas.typeMoviment)
     }
 
 }
