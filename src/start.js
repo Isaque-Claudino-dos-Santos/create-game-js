2841112e7df0f117a18986a02e55c8c08cf1848e
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-05-28 16:07:25 -0300

    add image source

--- src/start.js
+++ src/start.js
@@ -1,5 +1,6 @@
 function start() {
-    data.rect('player', 30, 30, 30, 30, 'blue')
+    data.image('player', 30, 30, 50, 50, 'ratata.png')
+        .source(0, 0, 32, 32)
         .speed(5)
         .moviment('press', 'w', 'd', 's', 'a')
         .save()
