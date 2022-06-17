b6fc1b1d40d2cab9d099128ff65ff3d730226638
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 19:16:08 -0300

    add colect fruit and collider player with hud

--- src/update.js
+++ src/update.js
@@ -1,4 +1,9 @@
+import hud from "../Modules/hud.js";
+import player from "../Modules/player.js";
 
 export default function update() {
+    player.collect('fruit')
+    hud.collider('player')
+
 
 }
