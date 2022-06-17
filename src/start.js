49ae0d035e312247e04ecd2a3b6aafcb32e9f051
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 16:24:48 -0300

    mv slime and fruit for module

--- src/start.js
+++ src/start.js
@@ -1,27 +1,10 @@
-function start() {
-    sprite.frames('slime', (f) => {
-        return {
-            left: f.create(0, 0, 16, 16),
-            right: f.create(16, 0, 16, 16),
-            stop: f.create(0, 16, 16, 16),
-            jump: f.create(16, 16, 16, 16)
-        }
-    })
+import fruit from '../Modules/fruit.js'
+import slime from '../Modules/slime.js'
 
 
-    data.image('player', 30, 30, 50, 50, 'slime.png')
-        .frames('slime')
-        .source('stop')
-        .speed(5)
-        .moviment('press', 'w', 'd', 's', 'a')
-        .save()
-
-    moviment.actionByKey('player', (mv) => {
-        mv.keydown.left = () => { sprite.alter('player', 'left') }
-        mv.keydown.right = () => { sprite.alter('player', 'right') }
-        mv.keyup.left = () => { sprite.alter('player', 'stop') }
-        mv.keyup.right = () => { sprite.alter('player', 'stop') }
-    })
-
-    console.log(data.find('player'))
+export default function start() {
+    slime.load()
+    slime.movimentActions()
+    
+    fruit.load()
 }
