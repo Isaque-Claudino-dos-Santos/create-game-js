ba09993fe3681eec358254c0190be3989de5a8c2
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 19:15:47 -0300

    load player,fruit,hud

--- src/start.js
+++ src/start.js
@@ -1,4 +1,9 @@
+import fruit from "../Modules/fruit.js";
+import hud from "../Modules/hud.js";
+import player from "../Modules/player.js";
 
 export default function start() {
-   
+    player.load()
+    fruit.load()
+    hud.load()
 }
