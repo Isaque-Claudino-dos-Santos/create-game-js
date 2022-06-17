5d5829056b52367e42e375087a73f31ffb466543
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 16:32:17 -0300

    cemantic

--- src/update.js
+++ src/update.js
@@ -1,7 +1,7 @@
 import fruit from "../Modules/fruit.js";
 
 export default function update() {
-    collider.set('hover', 'fruit', 'slime', () => {
+    collider.set('hover', 'slime', 'fruit', () => {
         fruit.randonPosition()
     })
 }
