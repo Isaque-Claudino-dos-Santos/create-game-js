1a8922a844287d80a557725751c44b022be4fcc0
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 14:44:15 -0300

    create new freme, define frame in player, define frame in moviment

--- src/start.js
+++ src/start.js
@@ -1,9 +1,27 @@
 function start() {
-    data.image('player', 30, 30, 50, 50, 'ratata.png')
-        .sources(0, 0, 32, 32)
+    sprite.frames('slime', (f) => {
+        return {
+            left: f.create(0, 0, 16, 16),
+            right: f.create(16, 0, 16, 16),
+            stop: f.create(0, 16, 16, 16),
+            jump: f.create(16, 16, 16, 16)
+        }
+    })
+
+
+    data.image('player', 30, 30, 50, 50, 'slime.png')
+        .frames('slime')
+        .source('stop')
         .speed(5)
         .moviment('press', 'w', 'd', 's', 'a')
         .save()
 
-        console.log(data.find('player'))
+    moviment.actionByKey('player', (mv) => {
+        mv.keydown.left = () => { sprite.alter('player', 'left') }
+        mv.keydown.right = () => { sprite.alter('player', 'right') }
+        mv.keyup.left = () => { sprite.alter('player', 'stop') }
+        mv.keyup.right = () => { sprite.alter('player', 'stop') }
+    })
+
+    console.log(data.find('player'))
 }
