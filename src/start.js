2c4a96a5767b2f2218259586db6174694fa182ba
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 19:33:25 -0300

    add animatio

--- src/start.js
+++ src/start.js
@@ -6,4 +6,16 @@ export default function start() {
     player.load()
     fruit.load()
     hud.load()
+
+
+    let i = 0
+    setInterval(() => {
+        i++
+        sprite.alter('player', i)
+        if (i == 10)
+            i = 0
+    }, 50)
+
+
 }
+
