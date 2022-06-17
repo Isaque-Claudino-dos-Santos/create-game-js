d7d9aabcf67200263acf4596722447664f4e7003
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 19:14:41 -0300

    module hud

---
+++ Modules/hud.js
@@ -0,0 +1,33 @@
+export default {
+    name: 'hud',
+    type: 'screen',
+
+    header() {
+        data.group(this.name, (d) => {
+            let hudSize = 30
+            return [
+                d.rect('top', 0, 0, WIDTH, hudSize).return(),
+                d.text('score', 10, 5, 20, 'SCORE: 0', 'white').return(),
+            ]
+        })
+    },
+
+    addScore() {
+        data.update(this.name, (datas) => {
+            let playerScore = data.find('player').score
+            datas[1].text = 'SCORE: ' + playerScore
+        })
+    },
+
+    collider(what) {
+        collider.set('solid', what, this.name)
+    },
+
+    load() {
+        this.header()
+        screen.create(this.name, this.name).save()
+        screen.visible(true, 'hud')
+    }
+
+
+}
