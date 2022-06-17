ead36cc94a83bb6237ee0f0b811d04887e1cdf89
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-06-17 19:18:23 -0300

    use method collider

--- src/update.js
+++ src/update.js
@@ -1,7 +1,6 @@
-import hud from "../Modules/hud.js";
 import player from "../Modules/player.js";
 
 export default function update() {
     player.collect('fruit')
-    hud.collider('player')
+    player.collider('hud')
 }
