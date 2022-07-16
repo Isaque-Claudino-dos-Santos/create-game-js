0092a81c507a09b0fcdb3cc1f3fde246a4c8bf22
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-15 22:14:13 -0300

    delete

--- default/bootstrep.js
+++
@@ -1,4 +0,0 @@
-const CANVAS = document.querySelector('#canvas-main')
-const CONTEXT = CANVAS.getContext('2d')
-const WIDTH = CANVAS.width
-const HEIGHT = CANVAS.height 
--- default/modules/rules.js
+++
@@ -1,7 +0,0 @@
-export default {
-    size: 25,
-    snakeColor: 'black',
-    snakeBodyColor: 'black',
-    fruitColor: 'black',
-    backgroundColor: '#64F391'
-}
